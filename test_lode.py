d8f8f5bccd7639b2bc40ffefb8597fa7a622e637
Louis Taylor <kragniz@gmail.com>
2014-08-20 23:47:31 +0100

    Fix formatting of test module

--- test_lode.py
+++ test_lode.py
@@ -2,27 +2,35 @@ import os
 
 import lode
 
+LODEFILE = 'lodefile'
+
+
 def teardown_function(function):
-    os.remove('lodefile')
+    os.remove(LODEFILE)
+
 
 def test_log_function_exists():
-    assert(lode.log('hello') == None)
+    assert(lode.log('hello') is None)
+
 
 def test_log_file_created():
     lode.log('created file')
-    assert(os.path.isfile('lodefile'))
+    assert(os.path.isfile(LODEFILE))
+
 
 def test_thing_is_logged():
     thing = 'thing with some different words'
 
     lode.log(thing)
 
-    with open('lodefile') as f:
+    with open(LODEFILE) as f:
         thing_found = False
         for line in f:
             if thing in line:
                 thing_found = True
+
     assert(thing_found)
+
 
 def test_five_things_are_logged():
     things = ('one', 'two', 'three', 'four')
@@ -30,9 +38,10 @@ def test_five_things_are_logged():
 
     lode.log(*things)
 
-    with open('lodefile') as f:
+    with open(LODEFILE) as f:
         things_found = False
         for line in f:
             if things_str in line:
                 things_found = True
+
     assert(things_found)
