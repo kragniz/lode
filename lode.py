44102f48d0f298be19e0d160c210ed78430005d4
Louis Taylor <kragniz@gmail.com>
2014-08-20 21:00:39 +0100

    Create the lodefile

--- lode.py
+++ lode.py
@@ -1,2 +1,3 @@
 def log(*args):
+    with open('lodefile', 'a') as f: pass
     print args
--- test_lode.py
+++ test_lode.py
@@ -1,4 +1,10 @@
+import os
+
 import lode
 
 def test_log_function():
     assert(lode.log('hello') == None)
+
+def test_log_file_created():
+    lode.log('created file')
+    assert(os.path.isfile('lodefile'))
