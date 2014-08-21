4292aeb503bebc2732560c80dcb46c202f0c01cc
Louis Taylor <kragniz@gmail.com>
2014-08-21 10:46:22 +0000

    Move formatting to seperate function

--- lode/__init__.py
+++ lode/__init__.py
@@ -5,10 +5,13 @@ import os
 def _get_caller_function():
     return inspect.stack()[2][3]
 
+def _format_items(items):
+    return ' '.join([str(item) for item in items])
+
 
 def log(*items, **kwargs):
     name = kwargs.get('name', 'lodefile')
 
     with open(name, 'a') as f: 
-        f.write(' '.join([str(item) for item in items]))
+        f.write(_format_items(items))
         f.write('\n')
