f44d9a458f61d1a320c3c6a67982920e3a0ebb55
Louis Taylor <kragniz@gmail.com>
2014-08-21 10:04:20 +0000

    Add python packaging

---
+++ README.rst
@@ -0,0 +1,8 @@
+====
+lode
+====
+
+.. image:: https://travis-ci.org/kragniz/lode.png?branch=master
+    :target: https://travis-ci.org/kragniz/lode
+
+Tiny and minimalistic logging utility module.
---
+++ setup.py
@@ -0,0 +1,28 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name='lode',
+    version='0.1.0',
+    author='Louis Taylor',
+    author_email='kragniz@gmail.com',
+    description=('Tiny and minimalistic logging utility'),
+    long_description=open("README.rst").read(),
+    license='GPL',
+    keywords='logging debugging',
+    url='https://github.com/kragniz/lode',
+    packages=['lode'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Topic :: Software Development :: Debuggers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
